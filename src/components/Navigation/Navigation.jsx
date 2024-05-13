ebc99fb5d846b49a6658c3eba6162ab97dc79027
Angela Antova <antovaangela@gmail.com>
2024-05-13 19:47:13 +0200

    responsiveness for nav and hero sections

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,26 +1,40 @@
+import { useState } from "react";
 import businessLogo from "../../assets/logo.png";
 import "./Navigation.css";
 import { Link } from "react-scroll";
 
 export const Navigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="Navigation">
       <div className="logo-container">
         <img src={businessLogo} alt="logo" />
       </div>
-      <nav className="navigation-container">
+
+      <nav className={`navigation-container ${isMenuOpen ? "open" : ""}`}>
+        <div className="hamburger-menu" onClick={ toggleMenu }>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+
         <ul className="nav-list">
           <li className="nav-element">
-            <Link to="hero-section">ДОМА</Link>
+            <Link to="hero-section" onClick={toggleMenu}>ДОМА</Link>
           </li>
           <li className="nav-element">
-            <Link to="menu">МЕНИ</Link>
+            <Link to="menu" onClick={toggleMenu}>МЕНИ</Link>
           </li>
           <li className="nav-element">
-            <Link to="about">ЗА НАС</Link>
+            <Link to="about" onClick={toggleMenu}>ЗА НАС</Link>
           </li>
           <li className="nav-element">
-            <Link to="contact">КОНТАКТ</Link>
+            <Link to="contact" onClick={toggleMenu}>КОНТАКТ</Link>
           </li>
         </ul>
       </nav>
