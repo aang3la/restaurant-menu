4524ad7d510870a6cadad687031d5df41f07d594
Angela Antova <antovaangela@gmail.com>
2024-06-05 20:47:18 +0200

    add meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import { About } from "./components/About/About";
 import { Footer } from "./components/Footer/Footer";
 import { Navigation } from "./components/Navigation/Navigation";
@@ -8,6 +10,14 @@ import { RecommendedFood } from "./components/RecommendedFood/RecommendedFood";
 function App() {
   return (
     <>
+      <Helmet>
+        <meta name="description" content="Website for a restaurant with menu" />
+        <meta property="og:title" content="Kaj stara lipa" />
+        <meta property="og:description" content="Website for a restaurant with menu" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://kajstaralipa.netlify.app/" />
+        <meta property="og:image" content="https://kajstaralipa.netlify.app/assets/logo-Uol14nM4.png" />
+      </Helmet>
       <Navigation />
       <HeroSection />
       <RecommendedFood />
